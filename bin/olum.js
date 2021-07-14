0ec7c0f20ddd6c5b910de39d032c6c9e5604775d
Eissa Saber <eissapk44@gmail.com>
2021-07-15 01:48:49 +0200

    updated version

--- bin/olum.js
+++ bin/olum.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
+/**
+ * @name Olum-cli.js
+ * @version 0.0.3
+ * @copyright 2021
+ * @author Eissa Saber
+ * @license MIT
+ */
 const commander = require("commander");
 const cmd = new commander.Command();
 const extra = require("fs-extra");
